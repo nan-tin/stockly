587578e411b56e967d5c8a4736f7e074562138c6
Takuma Tsutsumi <kumata0615@icloud.com>
2026-05-25 08:19:21 +0900

    categories CRUDの実装

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,54 @@
+class CategoriesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_category, only: %i[edit update destroy]
+
+  def index
+    @categories = current_user.groups.first.categories
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = current_user.groups.first.categories.build(category_params)
+
+    if @category.save
+      redirect_to categories_path, notice: "カテゴリーを作成しました"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @category.update(category_params)
+      redirect_to categories_path, notice: "カテゴリーを更新しました"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @category.destroy
+    redirect_to categories_path, notice: "カテゴリーを削除しました"
+  end
+
+  private
+
+  def current_group
+    current_user.groups.first
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+  
+  def set_category
+    @category = current_group.categories.find(params[:id])
+  end
+
+end
