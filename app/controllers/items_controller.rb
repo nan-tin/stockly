80372c13fe18c25746306d13c4af1876c22c0fcc
Takuma Tsutsumi <kumata0615@icloud.com>
2026-05-26 08:42:49 +0900

    items CRUDの実装完了

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,10 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_item, only: %i[edit update destroy]
 
   #includes は「N+1問題」を防ぐためのメソッド
   def index
-    @items = current_group.items.includes(:category)
+    @items = current_group.items.includes(:category).with_attached_image
   end
 
   def new
@@ -20,6 +21,22 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to items_path, notice: "アイテムを更新しました"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to items_path, notice: "アイテムを削除しました"
+  end
+
   private
 
   def item_params
@@ -32,6 +49,10 @@ class ItemsController < ApplicationController
       :memo,
       :image
     )
+  end
+
+  def set_item
+    @item = current_group.items.find(params[:id])
   end
 
 end
