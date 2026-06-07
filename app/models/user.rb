1d46982d5bd787132def9126ed0c260c38d6bcd0
Takuma Tsutsumi <kumata0615@icloud.com>
2026-06-07 21:37:38 +0900

    ユーザー登録時に初期グループを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,26 @@ class User < ApplicationRecord
   has_many :group_users, dependent: :destroy
   has_many :groups, through: :group_users
   has_many :inquiries, dependent: :destroy
+
+  after_create :create_default_group
+
+  private
+
+  def create_default_group
+    ActiveRecord::Base.transaction do
+      group = Group.create!(
+        invite_code: SecureRandom.hex(4)
+      )
+
+      GroupUser.create!(
+        user: self,
+        group: group,
+        display_name: email
+      )
+
+      ShoppingList.create!(
+        group: group
+      )
+    end
+  end
 end
