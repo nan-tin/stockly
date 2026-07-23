0c234221fc4628f2fd05ecb4b2265a1a64e2ba84
Takuma Tsutsumi <kumata0615@icloud.com>
2026-07-24 08:03:30 +0900

    グループオーナー移行処理を修正

--- db/migrate/20260720231223_add_owner_to_groups.rb
+++ db/migrate/20260720231223_add_owner_to_groups.rb
@@ -4,11 +4,27 @@ class AddOwnerToGroups < ActiveRecord::Migration[7.1]
                   :owner,
                   foreign_key: { to_table: :users }
 
-    Group.reset_column_information
+    execute <<~SQL
+      UPDATE groups
+      SET owner_id = (
+        SELECT group_users.user_id
+        FROM group_users
+        WHERE group_users.group_id = groups.id
+        ORDER BY group_users.created_at ASC, group_users.id ASC
+        LIMIT 1
+      )
+      WHERE owner_id IS NULL;
+    SQL
 
-    Group.find_each do |group|
-      owner = group.users.first
-      group.update_column(:owner_id, owner.id) if owner
+    groups_without_owner = select_value(<<~SQL).to_i
+      SELECT COUNT(*)
+      FROM groups
+      WHERE owner_id IS NULL;
+    SQL
+
+    if groups_without_owner.positive?
+      raise ActiveRecord::MigrationError,
+            "オーナーを設定できないグループが#{groups_without_owner}件あります"
     end
 
     change_column_null :groups, :owner_id, false
