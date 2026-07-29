37cef0c086a5b697c860e02cd3e85509d1ce85b6
Takuma Tsutsumi <kumata0615@icloud.com>
2026-07-29 22:19:26 +0900

    本番環境のメールURL生成設定を追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,6 +74,11 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :resend
+  
+  config.action_mailer.default_url_options = {
+    host: ENV.fetch("APP_HOST"),
+    protocol: "https"
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
